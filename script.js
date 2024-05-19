194cc086125f6b0326cdec329a9fd646351fdcb6
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-05-19 11:22:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+	let ans=new Array();
+	let i=0;
+	while(i<arr.length){
+		let temp=new Array();
+		let sum=arr[i];
+		
+		while(sum<=n){
+			temp.push(arr[i]);
+			i++;
+			sum+=arr[i];
+		}
+		ans.push(temp);
+	}
+	return ans
 };
 
 const n = prompt("Enter n: ");
